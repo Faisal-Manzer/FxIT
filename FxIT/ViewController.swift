97cc13114aaf36ef5e62dee15b622c532e738e94
Faisal Manzer <faisal_manzer@yahoo.in>
2018-04-28 23:59:31 +0530

    Intial Commit

---
+++ FxIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FxIT
+//
+//  Created by Faisal Manzer on 28/04/18.
+//  Copyright © 2018 Faisal Manzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

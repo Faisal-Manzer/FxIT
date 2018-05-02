b1f31c9cc85e9fe1f9b2d91ca355f1949db5cf2f
Faisal Manzer <faisal_manzer@yahoo.in>
2018-05-02 21:01:47 +0530

    Added actions for controls

--- FxIT/EditViewController.swift
+++ FxIT/EditViewController.swift
@@ -10,28 +10,78 @@ import UIKit
 
 class EditViewController: UIViewController {
     
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var pitchLabel: UILabel!
+    @IBOutlet weak var echoLabel: UILabel!
+    @IBOutlet weak var reverbLabel: UILabel!
+    
+    var effect = Effects()
     var recordedAudioURL: URL!
-
+    var speedSliderValue: Float = 1
+    var pitchSliderValue: Float = 0
+    var isEcho: Bool = false
+    var isReverb: Bool = false
+    enum SliderType: Int {
+        case speed = 0, pitch = 1
+    }
+    enum presetButton: Int {
+        case snail
+        case chipmunk
+        case echo
+        case rabbit
+        case vader
+        case reverb
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Actions
+    @IBAction func speedValueChange(_ sender: UISlider) {
+        let sliderValue = Float(sender.value)
+        speedLabel.text = "Speed = \(sliderValue)"
+        speedSliderValue = sliderValue
+        effect.speed = sliderValue
+    }
+  
+    @IBAction func pitchValueChange(_ sender: UISlider) {
+        let sliderValue = Float(sender.value)
+        pitchLabel.text = "Pitch = \(sliderValue)"
+        pitchSliderValue = sliderValue
+        effect.pitch = sliderValue
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func echoStateChange(_ sender: UISwitch) {
+        let state = sender.isOn
+        echoLabel.text = "Echo = \(state)"
+        isEcho = state
+        effect.echo = state
     }
-    */
-
+    
+    @IBAction func reverbStateChange(_ sender: UISwitch) {
+        let state = sender.isOn
+        reverbLabel.text = "Reverb = \(state)"
+        isReverb = state
+        effect.reverb = state
+    }
+    
+    @IBAction func presetButtonActio(_ sender: UIButton) {
+        switch(presetButton(rawValue: sender.tag)!) {
+        case .snail:
+            print("snail")
+        case .rabbit:
+            print("rabbit")
+        case .chipmunk:
+            print("chipmunk")
+        case .vader:
+            print("vader")
+        case .echo:
+            print("echo")
+        case .reverb:
+            print("reverb")
+        }
+    }
+    
 }

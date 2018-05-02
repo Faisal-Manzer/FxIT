d9d8c82ca58f46b1853f9d98558f13a68dfa4c21
Faisal Manzer <faisal_manzer@yahoo.in>
2018-05-03 00:29:18 +0530

    Finally working properly

--- FxIT/EditViewController.swift
+++ FxIT/EditViewController.swift
@@ -54,10 +54,27 @@ class EditViewController: UIViewController, AVAudioRecorderDelegate {
         case reverb
     }
     
+    struct Alerts {
+        static let DismissAlert = "Dismiss"
+        static let RecordingDisabledTitle = "Recording Disabled"
+        static let RecordingDisabledMessage = "You've disabled this app from recording your microphone. Check Settings."
+        static let RecordingFailedTitle = "Recording Failed"
+        static let RecordingFailedMessage = "Something went wrong with your recording."
+        static let AudioRecorderError = "Audio Recorder Error"
+        static let AudioSessionError = "Audio Session Error"
+        static let AudioRecordingError = "Audio Recording Error"
+        static let AudioFileError = "Audio File Error"
+        static let AudioEngineError = "Audio Engine Error"
+    }
+    
+    enum PlayingState { case playing, notPlaying }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupAudio()
     }
 
     //MARK: Actions
@@ -67,11 +84,11 @@ class EditViewController: UIViewController, AVAudioRecorderDelegate {
         case .speed:
 //            speedSliderValue = sliderValue
             effect.speed = sliderValue
-            speedLabel.text = "Speed:\(sliderValue)"
+//            speedLabel.text = "Speed:\(sliderValue)"
         case .pitch:
 //            pitchSliderValue = sliderValue
             effect.pitch = sliderValue
-            pitchLabel.text = "Pitch:\(sliderValue)"
+//            pitchLabel.text = "Pitch:\(sliderValue)"
         }
     }
     
@@ -81,11 +98,11 @@ class EditViewController: UIViewController, AVAudioRecorderDelegate {
         case .echo:
 //            isEcho = state
             effect.echo = state
-            echoLabel.text = "Echo:\(state)"
+//            echoLabel.text = "Echo:\(state)"
         case .reverb:
 //            isReverb = state
             effect.reverb = state
-            reverbLabel.text = "Reverb:\(state)"
+//            reverbLabel.text = "Reverb:\(state)"
         }
     }
    
@@ -123,14 +140,141 @@ class EditViewController: UIViewController, AVAudioRecorderDelegate {
     
     
     @IBAction func playButton(_ sender: UIButton) {
-        if !isPlaying {
-            playButton.setImage(UIImage(named: "stop"), for: .normal)
-        } else {
-            playButton.setImage(UIImage(named: "play"), for: .normal)
-        }
-        
         isPlaying = !isPlaying
+        if isPlaying {
+            playSound()
+            configureUI(.playing)
+        } else {
+            stopAudio()
+            configureUI(.notPlaying)
+        }
     }
     
+    //MARK: AudioFunctions
+    func setupAudio() {
+        // initialize (recording) audio file
+        do {
+            audioFile = try AVAudioFile(forReading: recordedAudioURL as URL)
+        } catch {
+            showAlert(Alerts.AudioFileError, message: String(describing: error))
+        }
+    }
+    
+    func playSound() {
+        let rate: Float? = effect.speed
+        let pitch: Float? = effect.pitch
+        let echo: Bool? = effect.echo
+        let reverb: Bool? = effect.reverb
+        
+        // initialize audio engine components
+        audioEngine = AVAudioEngine()
+        
+        // node for playing audio
+        audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attach(audioPlayerNode)
+        
+        // node for adjusting rate/pitch
+        let changeRatePitchNode = AVAudioUnitTimePitch()
+        if let pitch = pitch {
+            changeRatePitchNode.pitch = pitch
+        }
+        if let rate = rate {
+            changeRatePitchNode.rate = rate
+        }
+        audioEngine.attach(changeRatePitchNode)
+        
+        // node for echo
+        let echoNode = AVAudioUnitDistortion()
+        echoNode.loadFactoryPreset(.multiEcho1)
+        audioEngine.attach(echoNode)
+        
+        // node for reverb
+        let reverbNode = AVAudioUnitReverb()
+        reverbNode.loadFactoryPreset(.cathedral)
+        reverbNode.wetDryMix = 50
+        audioEngine.attach(reverbNode)
+        
+        // connect nodes
+        if echo == true && reverb == true {
+            connectAudioNodes(audioPlayerNode, changeRatePitchNode, echoNode, reverbNode, audioEngine.outputNode)
+        } else if echo == true {
+            connectAudioNodes(audioPlayerNode, changeRatePitchNode, echoNode, audioEngine.outputNode)
+        } else if reverb == true {
+            connectAudioNodes(audioPlayerNode, changeRatePitchNode, reverbNode, audioEngine.outputNode)
+        } else {
+            connectAudioNodes(audioPlayerNode, changeRatePitchNode, audioEngine.outputNode)
+        }
+        
+        // schedule to play and start the engine!
+        audioPlayerNode.stop()
+        audioPlayerNode.scheduleFile(audioFile, at: nil) {
+            
+            var delayInSeconds: Double = 0
+            
+            if let lastRenderTime = self.audioPlayerNode.lastRenderTime, let playerTime = self.audioPlayerNode.playerTime(forNodeTime: lastRenderTime) {
+                
+                if let rate = rate {
+                    delayInSeconds = Double(self.audioFile.length - playerTime.sampleTime) / Double(self.audioFile.processingFormat.sampleRate) / Double(rate)
+                } else {
+                    delayInSeconds = Double(self.audioFile.length - playerTime.sampleTime) / Double(self.audioFile.processingFormat.sampleRate)
+                }
+            }
+            
+            // schedule a stop timer for when audio finishes playing
+            self.stopTimer = Timer(timeInterval: delayInSeconds, target: self, selector: #selector(self.stopAudio), userInfo: nil, repeats: false)
+            RunLoop.main.add(self.stopTimer!, forMode: RunLoopMode.defaultRunLoopMode)
+        }
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            showAlert(Alerts.AudioEngineError, message: String(describing: error))
+            return
+        }
+        
+        // play the recording!
+        audioPlayerNode.play()
+    }
+    
+    @objc func stopAudio() {
+        
+        if let audioPlayerNode = audioPlayerNode {
+            audioPlayerNode.stop()
+        }
+        
+        if let stopTimer = stopTimer {
+            stopTimer.invalidate()
+        }
+        
+        configureUI(.notPlaying)
+        
+        if let audioEngine = audioEngine {
+            audioEngine.stop()
+            audioEngine.reset()
+        }
+        
+        isPlaying = false
+    }
+    
+    func configureUI(_ playState: PlayingState) {
+        switch playState {
+        case .playing:
+            playButton.setImage(UIImage(named: "stop"), for: .normal)
+        case .notPlaying:
+            playButton.setImage(UIImage(named: "play"), for: .normal)
+        }
+    }
+    
+    func connectAudioNodes(_ nodes: AVAudioNode...) {
+        for x in 0..<nodes.count-1 {
+            audioEngine.connect(nodes[x], to: nodes[x+1], format: audioFile.processingFormat)
+        }
+    }
+    
+    func showAlert(_ title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: Alerts.DismissAlert, style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
     
 }
